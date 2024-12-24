473c81f520f212061f5db00c770ef3cc898e9692
Benjamin Roget <benjamin.rogetpro@gmail.com>
2024-12-24 19:21:48 +0100

    Add Radix UI components and update SearchGrid to support modifying game

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -12,8 +12,9 @@ import { GameDetails } from '@/app/db/gameDetail';
 import Link from 'next/link';
 import { buttonVariants } from './ui/button';
 import { DownloadIcon } from 'lucide-react';
+import { EditGameForm } from './editGameForm';
 
-export const GameCard = async ({ game}: { game: GameDetails }) => {
+export const GameCard = async ({ game, modifquery}: { game: GameDetails, modifquery: string }) => {
     return (
         <Card key={game.id}>
               <CardHeader>
@@ -25,15 +26,19 @@ export const GameCard = async ({ game}: { game: GameDetails }) => {
                 
               </CardHeader>
               <CardContent>
-                <CardTitle>{game.name}</CardTitle>
+                <CardTitle>{game.name} ({game.path})</CardTitle>
                 <CardDescription dangerouslySetInnerHTML={{__html: game.description.length > 100 ? `${game.description.substring(0, 250)}...` : game.description}}>
                 </CardDescription>
               </CardContent>
               <CardFooter>
-                <div className='flex flex-row gap-3'>
+                  <div className='flex flex-row gap-3'>
+                
                   <Link className={buttonVariants({ variant: "outline" })} href={`/games/${game.id}`}>Click here</Link>
                   
-                    <Link className={buttonVariants({ variant: "outline" })} href={`/api/download/${game.id}`}><DownloadIcon/></Link>
+                  <Link className={buttonVariants({ variant: "outline" })} href={`/api/download/${game.id}`}><DownloadIcon/></Link>
+                
+                  <EditGameForm game={game} modifquery={modifquery} />
+                
                 </div>
               </CardFooter>
         </Card>
--- components/SearchGrid.tsx
+++ components/SearchGrid.tsx
@@ -2,16 +2,16 @@ import { GameDetails } from "@/app/db/gameDetail";
 import { GameCard } from "./GameCard";
 
 
-export default function SearchGrid({ games, query }: { games: GameDetails[], query: string }) {
+export default function SearchGrid({ games, query, modifquery }: { games: GameDetails[], query: string, modifquery: string  }) {
     return (
         <div className="px-5 py-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {games && games.length > 0 ? (games.map((game, index) => (
                 query ? (
                     game.name.toLowerCase().includes(query.toLowerCase()) ? (
-                        <GameCard key={index} game={game} />
+                        <GameCard key={index} game={game} modifquery={modifquery} />
                     ) : null
                 ) : (
-                    <GameCard key={index} game={game} />
+                    <GameCard key={index} game={game} modifquery={modifquery} />
                 )
             ))): (
                 <div className="text-center">No games</div>
