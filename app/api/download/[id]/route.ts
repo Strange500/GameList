3f7693d61b8cb0b4369589ede5e6496a25cdea68
Benjamin Roget <benjamin.rogetpro@gmail.com>
2024-12-24 15:16:54 +0100

    Refactor download route to streamline zip creation and response handling

--- app/api/download/[id]/route.ts
+++ app/api/download/[id]/route.ts
@@ -1,11 +1,7 @@
-import { getAllGames, getGame } from "@/app/db/gameDB";
+import { getGame } from "@/app/db/gameDB";
 import { GameDetails } from "@/app/db/gameDetail";
-import JSZip from "jszip";
-import fs from "fs";
 import archiver from 'archiver';
-import path, { join } from "path";
-import { NextResponse } from "next/server";
-import { Readable, Stream } from "stream";
+import { join } from "path";
 
 export async function GET(_: Request, { params }: { params: { id: string } }) {
   // Parse ID from params
@@ -51,13 +47,11 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
       zlib: { level: 0 } // Set the compression level
   });
 
-  // Create a stream to output the archive to response
-  const stream = new Readable().wrap(archive);
 
   // Set response headers
   const headers = {
       'Content-Type': 'application/zip',
-      'Content-Disposition': `attachment; filename=${id}.zip`,
+      'Content-Disposition': `attachment; filename=${game.name}.zip`,
   };
 
   // Error handling for archiver
@@ -69,51 +63,16 @@ export async function GET(_: Request, { params }: { params: { id: string } }) {
           },
           status: 500,
       });
-      stream.destroy(err); // Destroy the stream on error
       return response;
   });
 
   // Append directory to the archive
   archive.directory(gamePath, false);
 
-  // Finalize the archive and handle completion
-  archive.finalize()
-      .then(() => {
-          console.log('Archive has been finalized and is now ready for streaming.');
-      })
-      .catch((err: Error) => {
-          console.error('Finalization error: ', err);
-          stream.destroy(err);
-      });
+  // finalize the archive (ie we are done appending files but streams have to finish yet)
+  // 'close', 'end' or 'finish' may be fired right after calling this method so register to them beforehand
+  archive.finalize();
 
   // Return the response with the streaming archive
-  return new Response(stream as unknown as BodyInit, { headers });
-}
-
-
-
-/**
- * Adds all files from the specified folder to the provided JSZip instance.
- * 
- * @param folderPath - The path to the folder to zip.
- * @param zip - The JSZip instance to which files will be added.
- * @param basePath - The base path to maintain folder structure in the zip.
- */
-async function addFilesFromFolderToZip(folderPath: string, zip: JSZip, basePath: string = ''): Promise<void> {
-  const files = fs.readdirSync(folderPath);
-
-  for (const file of files) {
-      const fullPath = path.join(folderPath, file);
-      const stat = fs.statSync(fullPath);
-
-      if (stat.isDirectory()) {
-          // If it's a directory, recurse into it
-          await addFilesFromFolderToZip(fullPath, zip, path.join(basePath, file));
-      } else {
-          // If it's a file, read it and add it to the zip
-          const data = fs.readFileSync(fullPath);
-          const relativePath = path.join(basePath, file);
-          zip.file(relativePath, data);
-      }
-  }
+  return new Response(archive , { headers });
 }
