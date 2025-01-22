e44766a0663eae8b1f448a23a7147513dd68bfde
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-01-22 20:56:45 +0100

    Enhance search functionality in SearchGrid to include original game names and paths in query matching

--- components/SearchGrid.tsx
+++ components/SearchGrid.tsx
@@ -7,7 +7,11 @@ export default function SearchGrid({ games, query, modifquery }: { games: GameDe
         <div className="px-5 py-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
             {games && games.length > 0 ? (games.map((game, index) => (
                 query ? (
-                    game.name.toLowerCase().includes(query.toLowerCase()) ? (
+                    (game.name.toLowerCase().includes(query.toLowerCase()) ||
+                    game.name_original.toLowerCase().includes(query.toLowerCase()) ||
+                    game.path.toLowerCase().includes(query.toLowerCase()) 
+                    )
+                     ? (
                         <GameCard key={index} game={game} modifquery={modifquery} />
                     ) : null
                 ) : (
