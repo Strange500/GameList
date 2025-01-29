1f116e4826944f4b9d833cdafbdb13641f3e7bc5
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-01-29 22:04:36 +0100

    Refactor ModifTab component to use server actions for game modification

--- components/editGameFormWidget/ModifTab.tsx
+++ components/editGameFormWidget/ModifTab.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { GameDetails } from '@/app/db/interfaces/gameDetail';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '../ui/card';
 
@@ -7,56 +6,74 @@ import { Input } from '../ui/input';
 import { Button } from '../ui/button';
 import { Textarea } from '../ui/textarea';
 import React from 'react';
-import { modifyGame } from '@/app/db/gameDB';
+import { getGame, modifyGame } from '@/app/db/gameDB';
+import Form from 'next/form';
+import { revalidatePath } from 'next/cache';
 
 export const ModifTab = ({ game }: { game: GameDetails }) => {
+
+    const saveInfo = async (formData: FormData) =>  {
+        "use server";
+        const game = await getGame(parseInt(formData.get('id') as string));
+        if (!game) {
+            alert('The game you are trying to modify does not exist');
+            return;
+        }
+        const gameData = {
+            name: formData.get('name') as string,
+            name_original: formData.get('name_original') as string,
+            description: formData.get('description') as string,
+            released: new Date(formData.get('released') as string),
+            background_image: formData.get('background_image') as string
+        }
+        console.log(formData);
+        game.name = gameData.name;
+        game.name_original = gameData.name_original;
+        game.description = gameData.description;
+        game.released = gameData.released;
+        game.background_image = gameData.background_image;
+        await modifyGame(game);
+        revalidatePath('/');
+
+    }
 
 
     return (<Card >
                 <CardHeader>
                     <CardTitle>Game Info</CardTitle>
                 </CardHeader>
-                <CardContent className="space-y-2 ">
-                    
-                        <div className="space-y-1">
-                            <Label htmlFor="name">Name</Label>
-                            <Input id="name" defaultValue={game.name}  />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="name_original">Original Name</Label>
-                            <Input id="name_original" defaultValue={game.name_original} />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="description">Description</Label>
-                            <Textarea placeholder="Type your message here." id="description" defaultValue={game.description} className="w-full h-32" />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="released">Released</Label>
-                            <Input id="released" type="date" defaultValue={new Date(game.released).toISOString().split('T')[0]} />
-                        </div>
-                        <div className="space-y-1">
-                            <Label htmlFor="background_image">Background Image</Label>
-                            <Input id="background_image" defaultValue={game.background_image} />
-                        </div>
-                </CardContent>
-                
-                <CardFooter>
-                    <Button onClick={()=> {
-                        game.name = (document.getElementById("name") as HTMLInputElement)?.value || "";
-                        game.name_original = (document.getElementById("name_original") as HTMLInputElement)?.value || "";
-                        game.description = (document.getElementById("description") as HTMLInputElement)?.value || "";
-                        game.released = new Date((document.getElementById("released") as HTMLInputElement)?.value || "");
-                        game.background_image = (document.getElementById("background_image") as HTMLInputElement)?.value || "";
+                <Form action={saveInfo}>
+                    <CardContent className="space-y-2 ">
+                            <input type="hidden" name="id" value={game.id} />
 
-                        modifyGame(game).then(() => {
-                            setTimeout(() => {
-                                window.location.reload();
-                            }
-                            , 1000);
-                        }
-                        );
-                    }}>Save info</Button>
-                </CardFooter>
+
+                        
+                            <div className="space-y-1">
+                                <Label htmlFor="name">Name</Label>
+                                <Input id="name" name="name" defaultValue={game.name} />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="name_original">Original Name</Label>
+                                <Input id="name_original" name="name_original" defaultValue={game.name_original} />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="description">Description</Label>
+                                <Textarea placeholder="Type your message here." id="description" name="description" defaultValue={game.description} className="w-full h-32" />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="released">Released</Label>
+                                <Input id="released" type="date" name="released" defaultValue={new Date(game.released).toISOString().split('T')[0]} />
+                            </div>
+                            <div className="space-y-1">
+                                <Label htmlFor="background_image">Background Image</Label>
+                                <Input id="background_image" name="background_image" defaultValue={game.background_image} />
+                            </div>
+                    </CardContent>
+                    
+                    <CardFooter>
+                        <Button type='submit'>Save info</Button>
+                    </CardFooter>
+                </Form>
             </Card>);
     
 }
