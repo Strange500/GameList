9f47a464af75976d3d4371daaa7e0b8c27f492dd
Benjamin Roget <benjamin.rogetpro@gmail.com>
2025-01-29 21:34:18 +0100

    Enhance GameCard component with border styling; adjust layout for game details and download link visibility on hover

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -11,7 +11,7 @@ import { getGameBackgroundURI } from '@/app/db/gameDB';
 export const GameCard = async ({ game}: { game: GameDetails}) => {
     return (
       <Suspense fallback={<GameCardSkeleton />}>
-        <section key={game.id} className='group overflow-hidden h-80 shadow-lg bg-card relative hover:scale-105 ease-in duration-150 rounded-lg'>
+        <section key={game.id} className='group overflow-hidden h-80 shadow-lg bg-card relative hover:scale-105 ease-in duration-150 rounded-lg border border-black-200'>
           <Link href={`/games/${game.id}`}>
             <Image 
               src={await getGameBackgroundURI(game.id) || ''}
@@ -23,18 +23,21 @@ export const GameCard = async ({ game}: { game: GameDetails}) => {
           </Link>
     
             <div className='bg-black w-full  absolute  bottom-0  text-white p-2 place-content-evenly'>
-            <Link href={`/games/${game.id}`} className='flex flex-row justify-between'>
-                <h3 className='font-bold'>{game.name.slice(0, 55)}{game.name.length > 55 ? '...' : ''}</h3>
-                <i className='text-xs block mt-1 opacity-50'>{game.path.slice(0, 40)}{game.path.length > 55 ? '...' : ''}</i>
-            </Link>
-                <div className='flex flex-row justify-between mt-2'>
-                  <a href={`/api/download/${game.id}`}><DownloadIcon/></a>
-                  
+              <Link href={`/games/${game.id}`} className='flex flex-row justify-between'>
+                  <h3 className='font-bold'>{game.name.slice(0, 55)}{game.name.length > 55 ? '...' : ''}</h3>
                   <p className='text-sm'>{new Date(game.released).toLocaleDateString('fr-FR')}</p>
-                </div>
+                  
+              </Link>
+                
+                  <div className='flex flex-row justify-between mt-2 '>
+                    
+                    <i className='text-xs mt-1 opacity-50 hidden group-hover:block'>{game.path.slice(0, 40)}{game.path.length > 55 ? '...' : ''}</i>
+                    <a href={`/api/download/${game.id}`} className='hidden group-hover:block'><DownloadIcon/></a>
+                    
+                  </div>
             </div>
 
-            <div className='absolute top-2 right-2'>
+            <div className='absolute top-2 right-2 '>
               <EditGameForm game={game} />
             </div>
         </section>
